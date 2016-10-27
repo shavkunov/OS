86b94a75425a8fcbbf84aa6bb9408d9600d746c2
Mikhail <mv.shavkunov@yandex.ru>
2016-10-27 23:46:42 +0300

    attempt to fix slab

--- inc/slab_allocator.h
+++ inc/slab_allocator.h
@@ -5,20 +5,15 @@
 #include "stddef.h"
 #include "buddy.h"
 
-struct node {
-    struct node* next;
-};
 
 struct slabAllocator {
-    struct node* head;
-    struct slabAllocator *next;
-    struct slabAllocator *prev;
-    uint64_t allocatedNodes;
+    struct slabAllocator* head;
+    uint64_t dataSize;
 };
 
-void slabFree(struct slabAllocator* allocator, void* addr);
+void slabFree(struct slabAllocator** allocator, void* addr);
 void destroySlab(struct slabAllocator* allocator);
-void* slabAlloc(struct slabAllocator* allocator);
-struct slabAllocator* initSlab(uint64_t dataSize, uint64_t amount);
+void* slabAlloc(struct slabAllocator** allocator);
+struct slabAllocator* initSlab(uint64_t dataSize);
 
 #endif
